5a04f8f4a37621a661997a75580c139a52721b0d
LAKSHMI MAHATHI BHIMAVARAPU <luckyblm07@gmail.com>
2026-06-11 15:16:06 +0530

    Fix Lead import

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Lead = require("./models/Lead");
+const Lead = require("./models/lead");
 
 const app = express();
 
