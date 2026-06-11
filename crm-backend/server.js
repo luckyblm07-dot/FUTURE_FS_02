0d6fb989c9fb915e2ce506a64ed5f7551d4022dd
LAKSHMI MAHATHI BHIMAVARAPU <luckyblm07@gmail.com>
2026-06-11 15:44:46 +0530

    fix: added gitignore and removed env

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -98,6 +98,8 @@ app.delete("/leads/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
